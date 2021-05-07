b7b7051228fb01c29107ac2aff7d09319e9bbb20
zao <mr.zaotian@gmail.com>
2021-05-07 14:54:25 +0800

    一些组件

--- src/main.js
+++ src/main.js
@@ -7,14 +7,13 @@ import '@/element-ui'                         // api: https://github.com/ElemeFE
 import '@/icons'                              // api: http://www.iconfont.cn/
 import '@/element-ui-theme'
 import '@/assets/scss/index.scss'
-import PubSub from "pubsub-js";
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
-Vue.use(PubSub)
+
 // 非生产环境, 适配mockjs模拟数据                 // api: https://github.com/nuysoft/Mock
 if (process.env.NODE_ENV !== 'production') {
   require('@/mock')
--- src/views/modules/common/category-cascader.vue
+++ src/views/modules/common/category-cascader.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 
+  <!-- 
 使用说明：
 1）、引入category-cascader.vue
 2）、语法：<category-cascader :catelogPath.sync="catelogPath"></category-cascader>
@@ -10,7 +10,7 @@
   <div>
     <el-cascader
       filterable
-      clearable 
+      clearable
       placeholder="试试搜索：手机"
       v-model="paths"
       :options="categorys"
@@ -22,7 +22,7 @@
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
-
+import PubSub from 'pubsub-js'
 export default {
   //import引入的组件需要注入到对象中才能使用
   components: {},
@@ -30,10 +30,10 @@ export default {
   props: {
     catelogPath: {
       type: Array,
-      default(){
+      default() {
         return [];
-      }
-    }
+      },
+    },
   },
   data() {
     //这里存放数据
@@ -41,34 +41,35 @@ export default {
       setting: {
         value: "catId",
         label: "name",
-        children: "children"
+        children: "children",
       },
       categorys: [],
-      paths: this.catelogPath
+      paths: this.catelogPath,
     };
   },
-  watch:{
-    catelogPath(v){
+  watch: {
+    catelogPath(v) {
       this.paths = this.catelogPath;
     },
-    paths(v){
-      this.$emit("update:catelogPath",v);
-    }
+    paths(v) {
+      this.$emit("update:catelogPath", v);
+      PubSub.publish("catPath", v);
+    },
   },
   //方法集合
   methods: {
     getCategorys() {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/category"),
-        method: "get"
+        method: "get",
       }).then(({ data }) => {
         this.categorys = data.categories;
       });
-    }
+    },
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
     this.getCategorys();
-  }
+  },
 };
 </script>
